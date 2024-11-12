a5dd1fcf14bea54ed1eda300ad6d99902ef19deb
PhantomDD92 <lovelymonkey0612@gmail.com>
2024-11-12 18:47:26 +0200

    update tag inputs in content dialog

--- client/src/components/model/ModelContentDialog.jsx
+++ client/src/components/model/ModelContentDialog.jsx
@@ -37,23 +37,22 @@ const ModelContentDialog = ({ open, content, onCancel, onUpdate }) => {
     const handleOkClick = async () => {
         try {
             await form.validateFields();
-            const { medias, previews, ...params } = form.getFieldsValue();
-            console.log(medias, previews, params);
+            const { medias, previews, tags, ...params } = form.getFieldsValue();
+            const postTags = tags.replaceAll("#", " ").trim().split(/\s+/);
             let media = [{ name: mediaName, mode: mediaType }];
             let preview;
             if (previews && previews.length > 0) {
-                preview = { name: previewName,mode: previewType }
+                preview = { name: previewName, mode: previewType }
             }
-            onUpdate({ media, preview, ...params });
+            onUpdate({ media, preview, postTags, ...params });
         } catch (e) {
             console.error(e);
         }
     }
 
     useEffect(() => {
-        console.log("HERE :", open, content);
         if (open && content) {
-            const { image, platforms, media, preview, ...params } = content;
+            const { image, platforms, media, preview, postTags, ...params } = content;
             setPlatforms(platforms);
             let medias = [];
             let previews = [];
@@ -74,7 +73,7 @@ const ModelContentDialog = ({ open, content, onCancel, onUpdate }) => {
                 setPreviewName(preview.name);
                 previews = [preview.name];
             }
-            form.setFieldsValue({ medias, previews, platforms, ...params })
+            form.setFieldsValue({ medias, previews, platforms, tags: postTags.map(tag => `#${tag}`).join(" "), ...params })
         } else {
             form.resetFields();
             setMediaName();
@@ -175,8 +174,8 @@ const ModelContentDialog = ({ open, content, onCancel, onUpdate }) => {
                 <Form.Item name="title" label="Title" rules={[{ required: true }]}>
                     <Input />
                 </Form.Item>
-                <Form.Item name="postTags" label="Tags">
-                    <Select mode="tags" />
+                <Form.Item name="tags" label="Tags">
+                    <Input placeholder="#tag1 #tag2 #tag3" />
                 </Form.Item>
                 <Form.Item name="folder" label="Folder">
                     <Input />
