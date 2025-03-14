087232bb80619ff57b1716f1f6077bc04d325204
PhantomDD92 <lovelymonkey0612@gmail.com>
2025-03-14 11:52:48 +0200

    create nowpayment utils

---
+++ server/utils/nowpayment.js
@@ -0,0 +1,50 @@
+const axios = require('axios');
+
+const getApiStatus = async () => {
+  try {
+    await axios.get("https://api-sandbox.nowpayments.io/v1/status");
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
+const getAvailableCurrencies = async () => {
+  try {
+    const resp = await axios.get("https://api-sandbox.nowpayments.io/v1/currencies", { headers: { 'x-api-key': process.env.NOWPAYMENT_API_KEY } });
+    return resp.data?.currencies;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+const createPayment = async (agency, orderId, currency) => {
+  try {
+    const resp = await axios.post(
+      "https://api-sandbox.nowpayments.io/v1/payment",
+      {
+        "price_amount": 100,
+        "price_currency": "usd",
+        "pay_amount": 10,
+        "pay_currency": currency,
+        "ipn_callback_url": "https://modelvi.com/api/payment",
+        "order_id": `${orderId}`,
+        "order_description": `Modelvi payment from ${agency.name}`
+      },
+      { headers: { 'x-api-key': process.env.NOWPAYMENT_API_KEY } });
+      return res.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+const NowPaymentUtils = {
+  getApiStatus,
+  getAvailableCurrencies,
+  createPayment,
+};
+
+module.exports = NowPaymentUtils;
