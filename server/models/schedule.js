59ef5f6ef40bf693f146dc66855e8f7ab4b76c94
PhantomDD92 <lovelymonkey0612@gmail.com>
2025-05-07 14:11:10 +0800

    update schedule posting

--- server/models/schedule.js
+++ server/models/schedule.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { PostType } = require("../config/const");
 const { Schema, SchemaTypes } = mongoose;
 
 const ScheduleSchema = new Schema({
@@ -9,7 +10,7 @@ const ScheduleSchema = new Schema({
     title: { type: String, required: true },
     folder: { type: String },
     tags: [{ type: String }],
-    type: { type: Number, required: true },
+    type: { type: Number, required: true, default: PostType.FREE },
     price: { type: Number },
     results: [{ type: SchemaTypes.ObjectId, ref: "ScheduleResult" }],
     scheduledAt: { type: Date, required: true },
