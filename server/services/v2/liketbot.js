f9b0b25bac097d5e739f4f9c38b0fdb9f3a79c80
PhantomDD92 <lovelymonkey0612@gmail.com>
2025-08-04 20:39:02 +0800

    update like bot api

--- server/services/v2/liketbot.js
+++ server/services/v2/liketbot.js
@@ -74,7 +74,7 @@ const setAccountVerified = (botId) =>
   LikeBotModel.findByIdAndUpdate(botId, { $set: { verified: true } });
 
 const setLastError = (botId, lastError = "", disabled) =>
-  LikeBotModel.findByIdAndUpdate(botId, { $set: { lastError, status: !disabled } });
+  LikeBotModel.findByIdAndUpdate(botId, { $set: { lastError } });
 
 const setBotDevice = (botId, device) =>
   LikeBotModel.findByIdAndUpdate(botId, { $set: { device } });
