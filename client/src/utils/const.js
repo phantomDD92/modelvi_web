39ba63dd984979e713e65aba5fcd99e01aa936db
PhantomDD92 <lovelymonkey0612@gmail.com>
2025-08-22 18:31:17 +0800

    fix a bug

--- client/src/utils/const.js
+++ client/src/utils/const.js
@@ -5,6 +5,7 @@ export const SERVER_PATH = `${window.location.protocol}//${window.location.host}
 
 export const DEFAULT_REFRESH_TIMEOUT = 180000;
 export const DEFAULT_PAGE_SIZE = 20;
+export const LARGE_PAGE_SIZE = 100;
 export const DEFAULT_CURRENT_PAGE = 1;
 
 export const DEFAULT_POST_INTERVAL = 60;
